13cea8d70d6756ca89a7b5350df1bac1e088638a
r3tr0-Dev <45609267+r3tr0-Dev@users.noreply.github.com>
2019-01-01 23:01:21 -0500

    Add files via upload

--- gui.py
+++ gui.py
@@ -9,18 +9,22 @@ class MainUI():
         self.playBtnPos = (graphics.Point(self.width/2,self.height/2))
         self.hsBtnPos = (graphics.Point(self.width/2,self.height/2 + 100))
         self.quitBtnPos = (graphics.Point(self.width/2,self.height/2 + 200))
+        self.highScoreBoardPos = (graphics.Point(self.width/2,self.height/2 + 100))
 
         self.bgImg = 'assets/backgrounds/backgroundGame.png'
         self.titleImg = 'assets/UI/buttons/playBtn.png'
         self.playImg = 'assets/UI/buttons/playBtn.png'
         self.highScoreImg = 'assets/UI/buttons/highscoreBtn.png'
         self.quitImg = 'assets/UI/buttons/quitBtn.png'
+        self.highScoreBoardImg = 'assets/backgrounds/backgroundHighscore.png'
 
         self.mainBackground = graphics.Image(self.bgPos,self.bgImg)
         #self.title = graphics.Image(self.titlePos,self.titleImg) this will have it's own image etc
         self.playBtn = graphics.Image(self.playBtnPos,self.playImg) 
         self.highScoreBtn = graphics.Image(self.hsBtnPos,self.highScoreImg)
         self.quitBtn = graphics.Image(self.quitBtnPos,self.quitImg)
+        self.highScoreBoard = graphics.Image(self.highScoreBoardPos,self.highScoreBoardImg)
+        #self.highScoreTitle = graphics.Image(self.highScoreBoardPos,self.image) this will draw HIGHSCORE on top of the highscore board
 
         self.buttonWidth = self.playBtn.getWidth()/2
         self.buttonHeight = self.playBtn.getHeight()/2
@@ -29,14 +33,19 @@ class MainUI():
         self.highscoreBtnCenter = self.highScoreBtn.getAnchor()
         self.quitBtnCenter = self.quitBtn.getAnchor()
 
+        self.isScoreButtonPressed = False
+
     def draw(self,window):
         self.mainBackground.draw(window)
         self.playBtn.draw(window)
         self.highScoreBtn.draw(window)
         self.quitBtn.draw(window)
+        if self.isScoreButtonPressed:
+            self.highScoreBoard.draw(window)
         return
     
     def undraw(self):
+        self.mainBackground.undraw()
         self.playBtn.undraw()
         self.highScoreBtn.undraw()
         self.quitBtn.undraw()
@@ -56,10 +65,11 @@ class MainUI():
             print('play')
             return 'play'
         elif highScoreAbsX <= self.buttonWidth and highScoreAbsY <= self.buttonHeight:
-            print('score')
+            self.isScoreButtonPressed = True
             return 'score'
         elif quitAbsX <= self.buttonWidth and quitAbsY <= self.buttonHeight:
             quit()
+
 
     def display_main_menu(self, window):
         self.draw(window)
@@ -67,6 +77,8 @@ class MainUI():
         while run:
             click = window.getMouse()
             if self.returnButton(window, click) == 'score':
+                self.undraw()
+                self.draw(window)
                 # do score stuff
                 pass
             else:
