fc38c33b798f189c226e60a96e0313c8de670bba
nandatanda <39239764+nandatanda@users.noreply.github.com>
2019-01-02 13:19:13 -0500

    Obstacles

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import graphics
 import characters
+import obstacles
 import gui
 import time
 
-window = graphics.GraphWin('Bröther', 525, 700)
+window = graphics.GraphWin('Bröther', 525, 700, autoflush=False)
 moth = characters.Moth(window)
 ui = gui.MainUI(window)
+obstaclesList = list()
+obstaclesList.append(obstacles.Stalagtite(window))
 
 moth.draw(window)
+for obstacle in obstaclesList:
+    obstacle.draw(window)
 ui.display_main_menu(window)
 
 run = True
 while run:
     click = window.checkMouse()
     moth.update(window, click)
+    for obstacle in obstaclesList:
+        obstacle.update(window, moth)
     time.sleep(.05)
---
+++ obstacles.py
@@ -0,0 +1,35 @@
+import graphics
+from random import randint
+
+class Stalagtite():
+    def __init__(self, window):
+        self.x = window.getWidth()
+        self.y = 0
+        self.velocity = 10
+        self.image = graphics.Image(graphics.Point(self.x, self.y), 'assets/obstacles/stalagtite01.gif')
+        self.width = self.image.getWidth()
+        self.height = self.image.getHeight()
+        self.x += self.width / 2
+        self.randomize()
+
+    def update(self, window, player):
+        self.undraw()
+        self.move()
+        self.draw(window)
+
+    def undraw(self):
+        self.image.undraw()
+        return
+
+    def randomize(self):
+        self.velocity = randint(5, 10)
+        self.y += randint(-self.height / 4, self.height / 2)
+
+    def draw(self, window):
+        self.image = graphics.Image(graphics.Point(self.x, self.y), 'assets/obstacles/stalagtite01.gif')
+        self.image.draw(window)
+        return
+
+    def move(self):
+        self.x -= self.velocity
+        return
